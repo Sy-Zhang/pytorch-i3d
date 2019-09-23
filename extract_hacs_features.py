0625c837b7389ef0481d81e4752f8e998c6e6e0b
Sy-Zhang <szhang83@ur.rochester.edu>
2019-09-23 17:14:09 -0400

    Update

--- extract_hacs_features.py
+++ extract_hacs_features.py
@@ -10,6 +10,7 @@ import transforms
 import torch.nn.functional as F
 from pytorch_i3d import InceptionI3d
 from PIL import Image
+import glob
 import random
 random.seed(0)
 
@@ -25,14 +26,39 @@ parser.add_argument('-start', default=0, type=int)
 parser.add_argument('-end', default=6672, type=int)
 args = parser.parse_args()
 
+def readFlow(fn):
+    """ Read .flo file in Middlebury format"""
+    # Code adapted from:
+    # http://stackoverflow.com/questions/28013200/reading-middlebury-flow-files-with-python-bytes-array-numpy
+
+    # WARNING: this will work on little-endian architectures (eg Intel x86) only!
+    # print 'fn = %s'%(fn)
+    with open(fn, 'rb') as f:
+        magic = np.fromfile(f, np.float32, count=1)
+        if 202021.25 != magic:
+            print('Magic number incorrect. Invalid .flo file')
+            return None
+        else:
+            w = np.fromfile(f, np.int32, count=1)
+            h = np.fromfile(f, np.int32, count=1)
+            # print 'Reading %d x %d flo file\n' % (w, h)
+            data = np.fromfile(f, np.float32, count=2*int(w)*int(h))
+            # Reshape data into 3D array (columns, rows, bands)
+            # The reshape here is for visualization, the original code is (w,h,2)
+            return np.resize(data, (int(h), int(w), 2))
+
 def run(mode, root, load_model, save_dir, video_list_file, batch_size, fps = 24):
-    trans = torchvision.transforms.Compose([
-        transforms.GroupResize(256, interpolation=Image.BILINEAR),
-        transforms.GroupCenterCrop(224),
-        transforms.Stack4d(roll=False),
-        transforms.ToTorchFormatTensor4d(div=True),
-        transforms.GroupNormalize(mean=[.5, .5, .5],std=[.5, .5, .5])
-    ])
+    if mode == 'rgb':
+        trans = torchvision.transforms.Compose([
+            transforms.GroupResize(256, interpolation=Image.BILINEAR),
+            transforms.GroupCenterCrop(224),
+            transforms.Stack4d(roll=False),
+            transforms.ToTorchFormatTensor4d(div=True),
+            transforms.GroupNormalize(mean=[.5, .5, .5],std=[.5, .5, .5])
+        ])
+    else:
+        trans = torchvision.transforms.Compose([transforms.CenterCrop(224)])
+
     if mode == 'flow':
         i3d = InceptionI3d(400, in_channels=2)
     else:
@@ -69,14 +95,26 @@ def run(mode, root, load_model, save_dir, video_list_file, batch_size, fps = 24)
         # example: ffmpeg -i video_validation_0000051.mp4 -q:v 2 -f image2 output/image%5d.jpg
 
         # for philly
-        os.system('cp '+video_path+' '+frame_path)
-        duplicate_video_path = os.path.join(frame_path, video_path.split('/')[-1])
-        os.system('/home/v-yale/ffmpeg-4.2.1-amd64-static/ffmpeg -i ' + duplicate_video_path + ' -q:v 2 -f image2 -vf fps={} '.format(fps) + frame_path + '/image_%6d.jpg')
+        # os.system('cp '+video_path+' '+frame_path)
+        # duplicate_video_path = os.path.join(frame_path, video_path.split('/')[-1])
+        # os.system('/home/v-yale/ffmpeg-4.2.1-amd64-static/ffmpeg -i ' + duplicate_video_path + ' -q:v 2 -f image2 -vf fps={} '.format(fps) + frame_path + '/image_%6d.jpg')
 
         # for gpu07
-        # os.system('ffmpeg -i ' + video_path + ' -q:v 2 -f image2 -vf fps={} '.format(fps) + frame_path + '/image_%6d.jpg')
+        os.system('ffmpeg -i ' + video_path + ' -q:v 2 -f image2 -vf fps={} '.format(fps) + frame_path + '/image_%6d.jpg')
 
-        image_list = sorted(os.listdir(frame_path))[:-1]
+        if mode == 'flow':
+            os.system(
+                "python /home/v-yale/flownet2-pytorch/main.py "
+                "--inference --model FlowNet2 --save_flow --inference_dataset ImagesFromFolder "
+                "--inference_dataset_root {} --inference_dataset_iext jpg "
+                "--resume /home/v-yale/flownet2-pytorch/checkpoints/FlowNet2_checkpoint.pth.tar "
+                "--inference_batch_size 64".format(frame_path)
+            )
+        if mode == 'rgb':
+            image_list = sorted(glob.glob(frame_path+'/*.jpg'))[:-1]
+        else:
+            image_list = sorted(glob.glob(frame_path+'/*.flo'))[:-1]
+
         total_frames = len(image_list)
         if total_frames == 0:
             error_fid.write(video_path + '\n')
@@ -98,8 +136,13 @@ def run(mode, root, load_model, save_dir, video_list_file, batch_size, fps = 24)
             input_blobs = []
             num_sample = batch_size if i < n_batch-1 else n_feat-(n_batch-1) * batch_size
             for j in range(num_sample):
-                imgs = [Image.open(os.path.join(frame_path, image_list[k])) for k in
-                        range(sample_list[i * batch_size + j]-stride, sample_list[i * batch_size + j]+stride)]
+                if mode == 'rgb':
+
+                    imgs = [Image.open(os.path.join(frame_path, image_list[k])) for k in
+                            range(sample_list[i * batch_size + j]-stride, sample_list[i * batch_size + j]+stride)]
+                else:
+                    imgs = [readFlow(os.path) for k in
+                            range(sample_list[i * batch_size + j]-stride, sample_list[i * batch_size + j]+stride)]
                 imgs = trans(imgs)
                 input_blobs.append(imgs)
             input_blobs = torch.stack(input_blobs).permute(0, 4, 1, 2, 3).cuda()
--- transforms.py
+++ transforms.py
@@ -41,6 +41,37 @@ class GroupCenterCrop(object):
     def __call__(self, img_group):
         return [self.worker(img) for img in img_group]
 
+class CenterCrop(object):
+    """Crops the given seq Images at the center.
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, imgs):
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+        Returns:
+            PIL Image: Cropped image.
+        """
+        t, h, w, c = imgs.shape
+        th, tw = self.size
+        i = int(np.round((h - th) / 2.))
+        j = int(np.round((w - tw) / 2.))
+
+        return imgs[:, i:i+th, j:j+tw, :]
+
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
 
 class GroupRandomHorizontalFlip(object):
     """Randomly horizontally flips the given PIL.Image with a probability of 0.5
