ee2ce617250d74deef0ce547e661afec34877f3f
Songyang Zhang <szhang83@ur.rochester.edu>
2019-09-25 20:41:43 +0800

    Update

--- merge_npy_to_h5.py
+++ merge_npy_to_h5.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-npy_dir', default='/hdfs/resrchvc/v-yale/v-sozhan/Data/HACS-SF-RGB-Features/', type=str)
 parser.add_argument('-save_dir', default='/home/v-yale', type=str)
 parser.add_argument('-save_name', default='slowfast_features.h5', type=str)
-parser.add_argument('-video_list_file', default='videos.txt', type=str)
+parser.add_argument('-video_list_file', default='../videos.txt', type=str)
 args = parser.parse_args()
 
 if not os.path.isdir(args.save_dir):
@@ -18,10 +18,15 @@ f = h5py.File(os.path.join(args.save_dir, args.save_name), 'w')
 video_list = open(args.video_list_file).readlines()
 video_list = [item.strip() for item in video_list]
 
-pbar = tqdm(len(video_list))
-for video_name in video_list:
+pbar = tqdm(total=len(video_list))
+for video_name in video_list[1000:2000]:
     video_id = video_name.split('.')[0]
-    features = np.load(os.path.join(args.npy_dir,video_id+'.npy'))
+    try:
+        #print(os.path.join(args.npy_dir,video_id+'.npy'))
+        features = np.load(os.path.join(args.npy_dir,video_id+'.npy'))
+    except:
+        print('load error')
+        continue
     f.create_dataset(video_name, data=features)
     pbar.update(1)
 pbar.close()
