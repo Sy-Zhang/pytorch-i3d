1ce769fbdeab905c5a56bff82b544ed1ebf5e87e
Songyang Zhang <szhang83@ur.rochester.edu>
2019-09-26 12:19:37 +0800

    Update

--- merge_npy_to_h5.py
+++ merge_npy_to_h5.py
@@ -15,11 +15,14 @@ if not os.path.isdir(args.save_dir):
     os.mkdir(args.save_dir)
 f = h5py.File(os.path.join(args.save_dir, args.save_name), 'w')
 
+with open('/hdfs/resrchvc/v-yale/v-sozhan/Data/processed_videos.txt','r') as ff:
+    processed_vids =[l.strip() for l in ff.readlines()]
 video_list = open(args.video_list_file).readlines()
 video_list = [item.strip() for item in video_list]
+video_list = [vid for vid in video_list if vid.split('/')[-1].split('.')[0] not in processed_vids]
 
 pbar = tqdm(total=len(video_list))
-for video_name in video_list[1000:2000]:
+for video_name in video_list[0:1000]:
     video_id = video_name.split('.')[0]
     try:
         #print(os.path.join(args.npy_dir,video_id+'.npy'))
@@ -27,6 +30,6 @@ for video_name in video_list[1000:2000]:
     except:
         print('load error')
         continue
-    f.create_dataset(video_name, data=features)
+    f.create_dataset(video_id, data=features)
     pbar.update(1)
 pbar.close()
